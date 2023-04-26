195f5290e1ab2710018e4c7df6223e1b808a54de
meimeiwiser <79951744+meimeiwiser@users.noreply.github.com>
2023-04-26 15:36:41 -0700

    Create xcode project

---
+++ foodRestrictions/foodRestrictions/foodRestrictionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodRestrictionsApp.swift
+//  foodRestrictions
+//
+//  Created by Megan Wiser on 4/26/23.
+//
+
+import SwiftUI
+
+@main
+struct foodRestrictionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
